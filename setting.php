90785324f91a9ec14497903cd7e51d0818ba58ca
kav152 <kav152366@gmail.com>
2026-02-24 08:57:31 +0500

    вар 4.6.1

--- setting.php
+++ setting.php
@@ -2,7 +2,6 @@
 require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-/*
 
 return [
     'connections' => [
@@ -20,28 +19,6 @@ return [
             'dbname'   => $_ENV['DB_NAME_SQL'],
             'username' => $_ENV['DB_USER_SQL'],
             'password' => $_ENV['DB_PASSWORD_SQL'],
-            'charset'  => 'UTF-8'
-        ],
-    ]
-];*/
-
-
-return [
-    'connections' => [
-        'databaseMySQl' => [
-            'driver'   => 'mysql',
-            'host'     => 'localHost',
-            'dbname'   => 'a1138175_ConstructionAccounting',
-            'username' => 'a1138175_00',
-            'password' => 'pEdwuU3Z',
-            'charset'  => 'utf8mb4'
-        ],
-        'databaseSRV' => [
-            'driver'   => 'sqlsrv',
-            'host'     => '91.201.55.41',
-            'dbname'   => 'ConstructionAccounting_new12',
-            'username' => 'sa1',
-            'password' => 'SQLdostup123',
             'charset'  => 'UTF-8'
         ],
     ]
